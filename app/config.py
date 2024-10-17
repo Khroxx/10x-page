416f92ab38139ea251862f41292af45b395539dc
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-10-17 14:01:46 +0200

    pipenv file

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test:testpassword@localhost:3306/texsib_projekt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    SECRET_KEY = "ASDF345345GSDG"
+# zu test zwecken ohne .env
