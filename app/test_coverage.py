32e420e61383d2a09ec31ebce75865c18607f097
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-10-23 14:41:32 +0200

    pytest and coverage

--- app/test_coverage.py
+++ app/test_coverage.py
@@ -29,16 +29,19 @@ def test_get_table_data(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"Ziele" in response.data    # Inhalt des Templates (Title)
+    print(f"index templated recieved")
 
 def test_get_liste(client):
     response = client.get('/liste')
     assert response.status_code == 200
     assert b'Liste + Chart' in response.data
+    print(f"liste template recieved")
 
 def test_add_ziel_form(client):
     response = client.get('/add-ziel-form')
     assert response.status_code == 200
     assert b'Neues Ziel hinzu' in response.data
+    print(f"add_ziel template recieved")
 
 def test_submit_ziel(client):
     response = client.post('/submit-ziel', data={
@@ -51,6 +54,7 @@ def test_submit_ziel(client):
         'author': 'Test Author'
     })
     assert response.status_code == 302 
+    print(f"ziel submitted")
 
 def test_edit_ziel(client, app):
     with app.app_context():
@@ -71,6 +75,7 @@ def test_edit_ziel(client, app):
         assert response.status_code == 200
         json_data = response.get_json()
         assert json_data['message'] == 'Ziel aktualisiert'
+        print(f"Ziel edited")
 
 def test_delete_ziel(client, app):
     with app.app_context():
@@ -82,6 +87,7 @@ def test_delete_ziel(client, app):
         assert response.status_code == 200
         json_data = response.get_json()
         assert json_data['message'] == 'Ziel gelöscht'
+        print(f"Ziel deleted")
 
 def test_get_ziel_historie(client):
     ziel = Ziel(abteilung='IT', aussage='Test', kriterium='Test', bewertung=5, einschätzung='Gut', geändert=datetime.now(timezone.utc), kommentar='Test', author='Test')
@@ -91,8 +97,10 @@ def test_get_ziel_historie(client):
     response = client.get(f'/ziel/{ziel.id}/historie')
     assert response.status_code == 200
     assert b'Historie von' in response.data
+    print(f"ZielHistorie with ID recieved")
 
 def test_get_ziele_historie(client):
     response = client.get('/api/ziele_historie')
     assert response.status_code == 200
     assert 'application/json' in response.content_type
+    print(f"ZielHistorie recieved")
