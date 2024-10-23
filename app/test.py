4516cb9780690588a4ea4fd21f6e670e6c677204
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-10-23 12:44:10 +0200

    Update test.py

--- app/test.py
+++ app/test.py
@@ -3,19 +3,6 @@ from app import create_app, db
 from app.models import Ziel, ZielHistorie
 from datetime import datetime, timezone
 
-# @pytest.fixture
-# def client():
-#     app = create_app()
-#     app.config['TESTING'] = True
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    
-#     with app.test_client() as client:
-#         with app.app_context():
-#             db.create_all()
-#         yield client
-#         with app.app_context():
-#             db.drop_all()
-
 @pytest.fixture
 def app():
     app = create_app()
